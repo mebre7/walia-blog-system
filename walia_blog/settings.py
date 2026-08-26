51f08a204f07e41e4ab639bb8cd98577bc8550cf
mebratu <mebratucheka7@gmail.com>
2026-08-26 12:34:47 +0300

    folder change

--- walia_blog/settings.py
+++ walia_blog/settings.py
@@ -269,6 +269,7 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+    'CLOUDINARY_SECRET': os.getenv('CLOUDINARY_SECRET', 'walia-blog/'),
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
